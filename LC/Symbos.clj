4166baa2b098f909834ca531ce05b449c9e92cfc
Tharnid <jeffmsmithdev@gmail.com>
2015-07-30 00:14:11 -0400

    a little binding

--- LC/Symbos.clj
+++ LC/Symbos.clj
@@ -1,1 +1,11 @@
 (ns LCSymbols)
+
+(def developer "Terrell")
+
+developer
+
+;; let allows you to bind variable to another binding only within the context of the let
+
+(let [developer "Han Solo"] developer)
+
+developer
